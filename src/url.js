355dc6f3718fba322c272dc5de257af5fb196eff
Vaibhav4228 <vaibhavsharma88890@gmail.com>
2024-08-25 13:58:28 +0530

    updated

---
+++ src/url.js
@@ -0,0 +1,4 @@
+const IF = import.meta.env.VITE_IF || "https://blog-server-jqci.onrender.com/images/";
+const URL = import.meta.env.VITE_URL || "https://blog-server-jqci.onrender.com";
+
+export { IF, URL };
